3923194b0fa7b115c34e7313c325df4e60504bcf
iffybogus <36715727+iffybogus@users.noreply.github.com>
2023-03-28 20:19:19 +0000

    Added database connection

---
+++ database.py
@@ -0,0 +1,18 @@
+#import sqlalchemy
+#print(sqlalchemy.__version__)
+import pymysql
+from sqlalchemy import create_engine, text
+db_connection = "mysql+pymysql://3p5hvajgf7l768p9b6ya:pscale_pw_odbVEmFRLrPRd2dlLX7mfbDZKJH1N2K9p72SESoFnOk@us-east.connect.psdb.cloud/aitherapist?charset=utf8mb4"
+
+
+engine = create_engine(db_connection, 
+  connect_args = {
+    "ssl": {
+       "ssl_ca":"/etc/ssl/cert.pem"
+    }
+  })
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * from clients"))
+  print(result.all())
+  
